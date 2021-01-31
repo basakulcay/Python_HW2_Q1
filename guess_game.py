069e646b7c0ae582c3dbb2287869844abdfdb21d
Basak ulcay <55965449+basakulcay@users.noreply.github.com>
2021-01-31 17:45:37 +0300

    Update guess_game.py

--- guess_game.py
+++ guess_game.py
@@ -1,63 +1,48 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Mon Jan 25 20:58:59 2021
+Created on Sun Jan 31 17:39:40 2021
 
 @author: basakulcay
 """
-
-
 import random
-
 def main():
+    
+
     again='y'
     
-
     while again=='y':
-        count=0
-        print('I have a number between 1 to 1000.')
-        print('Can you guess my number?')
-        print('Please type your first guess') 
-        
-        number=random.randint(1, 1000)
-        #print(number)
+          count=0
+          print("I have a number between 1 to 1000.")
+          print("Can you guess my number?")
+          guess=int(input("Please type your first guess:"))
     
-        guess=int(input(''))
-       
-        while guess !='':
-            if  guess>number:
-                print('Too high, try again!')
-                count+=1
-                #print('count:',count)
-                guess=int(input(''))
-                
-                
-            elif guess<number:
-                print('Too low, try again!')
-                count+=1
-                #print('count:',count)
-                guess=int(input(''))
+    
+          number=random.randint(1,1000)
+        
+    
+          while guess !='':
+              if     number > guess:
+                     count+=1
+                     guess=int(input("Too low.Try again:"))
+                     
+              elif   number < guess:
+                     count+=1
+                     guess=int(input("Too high.Try again:"))
+                     
+              else:
+                    count+=1 
+                    if count>10 :
+                       print("You should be able to do better!!!")
+                    elif count==10:
+                       print("Ahah! You know the secret!") 
+                    else:
+                       print("Either you know the secret or you got lucky!") 
+                  
+                    again=str(input("Excellent!! You guessed the number!!! Would you like to play again (y or n)?"))
+                    break
+                  
 
-                
-            elif guess==number:
-                count+=1
-                if count==10:
-                    print('Ahah!You know the secret!')
-                elif count<10:
-                    print('Either you know the secret or you got lucky')
-                else:
-                    print('You can do better')
-                      
-                print('Excellent!! You guessed the number!!!!')
-                print('Would you like to try again? (y or n)')
-                
-                #print('count:',count)
-                again=str(input(''))
-                break
-            
-            else:
-                print('You entered an invalid value')
-                
-  
+
 main()
 
